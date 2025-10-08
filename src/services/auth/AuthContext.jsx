2df9ec3a2562cd8dad6bd3096937001be2885e44
Lucas Luppi <lucasgluppi@gmail.com>
2025-10-08 20:54:28 -0300

    agrego expiracion del token (falta redirección al login)

--- src/services/auth/AuthContext.jsx
+++ src/services/auth/AuthContext.jsx
@@ -1,35 +1,71 @@
-import { createContext, useContext, useState, useEffect } from 'react';
+import { createContext, useContext, useState, useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import { Navigate } from "react-router-dom";
+import { notifyMissingFields } from "../../pages/notification/notification";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   // Inicializo directamente con función para leer localStorage
-  const [token, setToken] = useState(() => localStorage.getItem('token') || '');
+  const [token, setToken] = useState(() => localStorage.getItem("token") || "");
   const [user, setUser] = useState(() => {
-    const userStr = localStorage.getItem('user');
+    const userStr = localStorage.getItem("user");
     return userStr ? JSON.parse(userStr) : null;
   });
+  const [redirectToLogin, setRedirectToLogin] = useState(false);
 
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    // Verificar validez y expiración del token
+    const checkToken = () => {
+      if (!token) {
+        logout();
+        return;
+      }
+      try {
+        const decodedToken = jwtDecode(token);
+        const currentTime = Date.now() / 1000; // jwt exp está en segundos
+
+        if (decodedToken.exp < currentTime) {
+          notifyMissingFields(
+            "Su sesión ha expirado. Por favor, inicie sesión nuevamente."
+          );
+          logout();
+        }
+      } catch (error) {
+        notifyMissingFields(
+          "Su sesión es inválida. Por favor, inicie sesión nuevamente."
+        );
+        logout();
+      }
+    };
+
+    checkToken();
+
+    // Verificación periódica cada 5 min
+    const interval = setInterval(checkToken, 5 * 60 * 1000);
     setLoading(false);
-  }, []);
+    return () => clearInterval(interval);
+  }, [token]);
 
   const login = ({ token, user }) => {
     setToken(token);
     setUser(user);
-    localStorage.setItem('token', token);
-    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem("token", token);
+    localStorage.setItem("user", JSON.stringify(user));
+    setRedirectToLogin(false);
   };
 
   const logout = () => {
-    setToken('');
+    setToken("");
     setUser(null);
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    setRedirectToLogin(true);
   };
 
+  // if (redirectToLogin) return <Navigate to="/login" replace />; Esto hay que crear un nuevo componente o envolver AuthProvider dentro de Routes
   return (
     <AuthContext.Provider
       value={{
