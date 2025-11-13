968b06985455802aa2378a80bc2469690b3c0bf3
Lucas Luppi <lucasgluppi@gmail.com>
2025-11-13 12:02:52 -0300

    corrección del botón ver detalles

--- src/features/publications/publicationCard/PublicationCard.jsx
+++ src/features/publications/publicationCard/PublicationCard.jsx
@@ -1,41 +1,73 @@
-import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useAuth } from '../../../services/auth/AuthContext';
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useAuth } from "../../../services/auth/AuthContext";
 
-const PublicationCard = ({ id, title, description, img, price, status, brand, city, category, subCategory, id_seller }) => {
-    const navigate = useNavigate();
-    const { user } = useAuth();
-    
+const PublicationCard = ({
+  id,
+  title,
+  description,
+  img,
+  price,
+  status,
+  brand,
+  city,
+  category,
+  subCategory,
+  id_seller,
+}) => {
+  const navigate = useNavigate();
+  const { user } = useAuth();
 
-    const handleCardClick = () => {
-        const publicacion = { id, title, description, img, price, status, brand, city, category, subCategory }
+  const handleButtonClick = (event) => {
+    event.stopPropagation(); //Probar borrando esta linea si surge algún error.
 
-        navigate(`/catalogo/${id}`, {
-            state: { publicacion }
-        })
-    }
+    const publicacion = {
+      id,
+      title,
+      description,
+      img,
+      price,
+      status,
+      brand,
+      city,
+      category,
+      subCategory,
+    };
 
-    const handleBuyClick = (event) => {
-        event.stopPropagation();
-        
-        if (user.id === id_seller) {
-            toast.info("No podés comprar tu propia publicación.");
+    navigate(`/catalogo/${id}`, {
+      state: { publicacion },
+    });
+  };
 
-            return;
-        }
+  //   const handleBuyClick = (event) => {
+  //     event.stopPropagation();
 
-        const publicacion = { id, title, description, img, price, status, brand, city }
+  //     if (user.id === id_seller) {
+  //       toast.info("No podés comprar tu propia publicación.");
 
-        navigate(`/catalogo/${id}/purchase-details`, {
-            state: { publicacion }
-        })
-    }
+  //       return;
+  //     }
 
+  //     const publicacion = {
+  //       id,
+  //       title,
+  //       description,
+  //       img,
+  //       price,
+  //       status,
+  //       brand,
+  //       city,
+  //     };
 
-    return (
-        <div
-            className="
+  //     navigate(`/catalogo/${id}/purchase-details`, {
+  //       state: { publicacion },
+  //     });
+  //   };
+
+  return (
+    <div
+      className="
                 relative
                 group
                 w-[250px]
@@ -46,26 +78,29 @@ const PublicationCard = ({ id, title, description, img, price, status, brand, ci
                 rounded-[10px] 
                 shadow-[5px_5px_5px_rgba(0,0,0,0.25)]
                 "
-            onClick={handleCardClick}
-        >
-            <img
-                src={img}
-                alt="imagen de Producto"
-                className="w-full h-[240px] bg-[#fff] rounded-t-[7px] object-contain"
-            />
-            <div className="px-[15px] py-[3px] text-left">
-                <span className="inline-block bg-[#D2D2D2] rounded-[7px] px-[15px] py-0.5 mt-[10px] mb-[8px] text-[0.80rem] text-[black]">
-                    {category?.CategoryName}
-                </span>
-                <h3 className="text-[1.3rem] font-semibold leading-tight pb-[4px] mb-[2px] ">{title}</h3>
-                <p className="text-2 pb-[5px] m-[0]">{city?.Province?.Name}, {city?.Name}</p>
-                <p className="text-[1.7rem] pb-[5px] m-[0] font-poppins">${price}</p>
-                <p className="text-[1.25rem] pb-[8px] m-[0]">{status}</p>
-            </div>
+    >
+      <img
+        src={img}
+        alt="imagen de Producto"
+        className="w-full h-[240px] bg-[#fff] rounded-t-[7px] object-contain"
+      />
+      <div className="px-[15px] py-[3px] text-left">
+        <span className="inline-block bg-[#D2D2D2] rounded-[7px] px-[15px] py-0.5 mt-[10px] mb-[8px] text-[0.80rem] text-[black]">
+          {category?.CategoryName}
+        </span>
+        <h3 className="text-[1.3rem] font-semibold leading-tight pb-[4px] mb-[2px] ">
+          {title}
+        </h3>
+        <p className="text-2 pb-[5px] m-[0]">
+          {city?.Province?.Name}, {city?.Name}
+        </p>
+        <p className="text-[1.7rem] pb-[5px] m-[0] font-poppins">${price}</p>
+        <p className="text-[1.25rem] pb-[8px] m-[0]">{status}</p>
+      </div>
 
-            <button
-                onClick={handleBuyClick}
-                className="
+      <button
+        onClick={handleButtonClick}
+        className="
                     bg-[#9D9B8F] 
                     text-[#fff] 
                     font-bold
@@ -78,11 +113,12 @@ const PublicationCard = ({ id, title, description, img, price, status, brand, ci
                     opacity-0 
                     transition-all
                     duration-300
-                    group-hover:opacity-100">
-                Comprar
-            </button>
-        </div>
-    )
-}
+                    group-hover:opacity-100"
+      >
+        Ver detalles
+      </button>
+    </div>
+  );
+};
 
-export default PublicationCard
+export default PublicationCard;
