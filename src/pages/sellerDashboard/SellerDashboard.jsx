2658328bfacba3523c5b4b70c1cab1b6a5c3df27
Facundo Mandon <facundo-mandon@hotmail.com>
2026-02-11 20:22:45 -0300

    Implemento cambios segun vendedor en el SellerDashboard (se cuentan la cantidad de pedidos recibidos y publicaciones totales de cada uno)

--- src/pages/sellerDashboard/SellerDashboard.jsx
+++ src/pages/sellerDashboard/SellerDashboard.jsx
@@ -31,11 +31,35 @@ const SellerDashboard = ({ onRefresh }) => {
         fetchPublications();
     }, [user?.seller?.id]);
 
+
+const [DashboardData, setDashboardData] = useState(null);
+
+useEffect(() => {
+    const fetchDashboardData = async () => {
+        try {
+            const response = await fetch(`http://localhost:3000/seller/dashboard/${user.seller.id}`);
+
+            if (!response.ok) {
+                throw new Error("Error al cargar dashboard data");
+            }
+
+            const data = await response.json();
+            setDashboardData(data);
+
+        } catch (error) {
+            console.error("Error al obtener dashboard data", error);
+        } 
+    };
+
+    fetchDashboardData();
+
+}, []);
+
+
+
     const dashboardData = {
         totalSales: 150,
-        pendingOrders: 10,
         newMessages: 5,
-        activePosts: 25,
     };
 
     const cardVariants = {
@@ -106,7 +130,7 @@ const SellerDashboard = ({ onRefresh }) => {
                                         Pedidos Pendientes
                                     </h2>
                                     <p className="text-4xl text-[#401809] mt-2 hover:text-[#FFE0C4] transition-colors duration-300">
-                                        {dashboardData.pendingOrders}
+                                        {DashboardData?.pendingOrders}
                                     </p>
                                 </div>
                             </div>
@@ -142,7 +166,7 @@ const SellerDashboard = ({ onRefresh }) => {
                                         Publicaciones Activas
                                     </h2>
                                     <p className="text-4xl text-[#401809] mt-2 hover:text-[#FFE0C4] transition-colors duration-300">
-                                        {dashboardData.activePosts}
+                                        {DashboardData?.activePosts}
                                     </p>
                                 </div>
                             </div>
